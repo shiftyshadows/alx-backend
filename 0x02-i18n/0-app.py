fa368ba4f77c68b1c550375495ef3f88b1661d66
shiftyshadows <shifty.shadows@hotmail.com>
2025-01-24 16:36:08 +0300

    0. Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
+"""
+Flask application to serve a basic HTML page.
+
+This script initializes a Flask application and defines a route
+to render an HTML template.
+"""
+
 from flask import Flask, render_template
 
+# Initialize the Flask application
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
+    """
+    Home route that renders the '0-index.html' template.
+
+    Returns:
+        str: Rendered HTML content of the '0-index.html' template.
+    """
     return render_template('0-index.html')
 
-
 if __name__ == '__main__':
+    """
+    Run the Flask application in debug mode when the script is executed.
+    """
     app.run(debug=True)
